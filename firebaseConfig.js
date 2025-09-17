5ae9b41e80ee556a354b8811923449437972868d
JohnKenneth16 <johnkenethlibrado@gmail.com>
2025-09-17 08:58:12 +0800

    Create Goals

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+   apiKey: "AIzaSyDfge-sCki_b_B73mZSHfqRedPUKKY49-I",
+  authDomain: "smartassist-5da80.firebaseapp.com",
+  projectId: "smartassist-5da80",
+  storageBucket: "smartassist-5da80.firebasestorage.app",
+  messagingSenderId: "816438512300",
+  appId: "1:816438512300:web:f3fa63517ebffb8727ad09",
+  measurementId: "G-14HXNG71L7"
+};
+
+const app = initializeApp(firebaseConfig);
